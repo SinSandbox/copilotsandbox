d9aa1be8a98b6e99cc55f25cf6faffe4a561f1d7
Mona Sin <mosin@microsoft.com>
2025-10-23 10:22:43 -0500

    remove all files

--- README.md
+++
@@ -1,3 +0,0 @@
-Sandbox for testing out Copilot features. Feel free to modify to whatever codebase and language for testing and knowledge skilling.
-
-Have fun!!
--- testfile.js
+++
@@ -1,5 +0,0 @@
-function calculateDaysBetweenDates(begin, end) {
-    const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-    const diffInMs = Math.abs(end - begin);
-    return Math.round(diffInMs / oneDay);
-}
